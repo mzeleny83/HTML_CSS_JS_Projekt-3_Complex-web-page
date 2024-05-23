990218632ceee05a08b3d84961d7d61d1e7145d9
mzeleny83 <110114227+mzeleny83@users.noreply.github.com>
2024-05-23 12:05:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+<script>
+  function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+  }
+
+  document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get password inputs
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+
+    // Check if passwords match
+    if (password1 !== password2) {
+      document.getElementById("passwordMismatch").style.display = "block";
+    } else {
+      document.getElementById("passwordMismatch").style.display = "none";
+      // Submit form if passwords match
+      this.submit();
+    }
+  });
+
+  // Show/Hide Scroll to Top Button
+  window.addEventListener("scroll", function() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.documentElement.scrollTop > 100) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList}
+
+      </script>
